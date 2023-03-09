d5093913c92ad366dd9a482f32d21b3829a64e7c
Ricky.Wang <61725067+68748678@users.noreply.github.com>
2023-03-09 17:50:28 +0800

    Create zh_CN.ts

---
+++ Python3-Learning/desktop_application/zh_CN.ts
@@ -0,0 +1,126 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en_US">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Preferences</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>General</source>
+        <translation>常规</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Language:</source>
+        <translation>显示语言:</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <location filename="backend_1.ui" line="0"/>
+        <source>English</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>简体中文</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Configuration Storage</source>
+        <translation>配置文件存储位置</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Path:</source>
+        <translation>路径:</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Open containing folder</source>
+        <translation>打开所在文件夹</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Open</source>
+        <translation>打开</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Change</source>
+        <translation>更改</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Run on system startup</source>
+        <translation>开机启动</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Auto backup</source>
+        <translation>自动备份</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Enhanced tray menu</source>
+        <translation>托盘菜单</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Restore Default</source>
+        <translation>恢复默认</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Interface</source>
+        <translation>界面</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Radio</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Interface font:</source>
+        <translation>界面字体:</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Theme color:</source>
+        <translation>主题颜色:</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Tray icon:</source>
+        <translation>托盘图标:</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Auto</source>
+        <translation>自动</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <location filename="backend_1.ui" line="0"/>
+        <source>PushButton</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>Setup</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="backend_1.ui" line="0"/>
+        <source>About</source>
+        <translation>关于</translation>
+    </message>
+</context>
+</TS>
